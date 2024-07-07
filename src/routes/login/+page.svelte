1aa68f44a461e32a01ed5edaa6eb5195b436cd7f
androidinflow <zakharname0111@gmail.com>
2024-07-07 02:00:12 +0200

    first commit

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { browser } from "$app/environment";
+
+  // Define the data variable
+  export let data;
+
+  // Define the gotoAuthProvider function
+  function gotoAuthProvider() {
+    // Save the state in the cookie
+    if (browser) {
+      document.cookie = `state=${data?.authProviderState}`;
+    }
+
+    // Redirect the user to the OAuth login for Github
+    window.location.href = data.authProviderRedirect || "";
+  }
+</script>
+
+// src/routes/login/+page.svelte
+
+<button on:click={gotoAuthProvider}>Login with github</button>
